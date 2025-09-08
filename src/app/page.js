2c72f38715b76787bc4b42df369ef7ead006c928
Jenas Anton <jenasav010@gmail.com>
2025-09-08 15:35:00 +0530

    home done , responsice

--- src/app/page.js
+++ src/app/page.js
@@ -1,40 +1,47 @@
 import ZoomParallax from "./Components/ZoomParallax";
 import TextType from "./Components/TextType";
-import Skills from "./Horizontal_Pages/Skills_page/Skills";
 import HorizontalScroll from "./Horizontal_Pages/HorizontalScroll";
 import Header from "./Components/Navigation/Header";
 
 export default function Page() {
   return (
     <main>
-      <div className="snap-y snap-mandatory">
-      <Header />
-      <section id = "home" className="snap-start">
-        <div>
-        <h1  className="name "> Jenas Anton Vimal </h1>
-        <div className="type">
-          <TextType 
-            text={["Text typing effect", "for your websites", "Happy coding!"]}
-            typingSpeed={75}
-            pauseDuration={1500}
-            showCursor={true}
-            cursorCharacter="_"
-          />
-        </div>
-      
-      <ZoomParallax 
-        imageSrc="/computer.png"
-        imageAlt="computer"
-        maxScale={6.5}
-        originX={0.45}
-        originY={0.3}
-      />
-      </div>
-      </section>
+      <div>
+        <Header />
+        <section
+          id="home"
+          className="flex flex-col items-center justify-center gap-10"
+        >
+          {/* Wrap all inner content in a column with gap */}
+          <div className="flex flex-col items-center gap-8">
+            <h1 className="name">Jenas Anton Vimal</h1>
 
-      <HorizontalScroll />
+            <div className="type">
+              <TextType
+                text={[
+                  "Text typing effect",
+                  "for your websites",
+                  "Happy coding!",
+                ]}
+                typingSpeed={75}
+                pauseDuration={1500}
+                showCursor={true}
+                cursorCharacter="_"
+              />
+            </div>
+
+            <ZoomParallax
+              imageSrc="/computer.png"
+              imageAlt="computer"
+              maxScale={6.5}
+              originX={0.45}
+              originY={0.3}
+            />
+          </div>
+        </section>
+
+        <HorizontalScroll />
       </div>
     </main>
   );
 }
-     
